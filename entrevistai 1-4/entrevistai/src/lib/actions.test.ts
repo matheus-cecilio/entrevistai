e3e96f982f81c205618671a6223c48e14e356db1
Matheus Cecilio Barros <matheus.barros06@cs.cruzeirodosul.edu.br>
2025-08-07 09:11:57 -0300

    Delete entrevistai 1-4/entrevistai directory

--- entrevistai 1-4/entrevistai/src/lib/actions.test.ts
+++
@@ -1,6 +0,0 @@
-import { startInterviewAction } from './actions';
-
-test('startInterviewAction retorna erro para input inválido', async () => {
-  const result = await startInterviewAction({ jobRole: '', techStack: '' });
-  expect(result.success).toBe(false);
-});
